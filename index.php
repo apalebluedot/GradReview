be20d95fb1a5d4f0dbaf202784484932c033ffda
apalebluedot <bradley.barnes21@gmail.com>
2013-04-05 00:33:23 -0500

    Styled

---
+++ index.php
@@ -0,0 +1,13 @@
+<!Doctype HTML>
+	<html>
+		<head>
+			<title>GradReview</title>
+			<link rel="stylesheet" href="style.css">
+		</head>
+		<body>
+			<div>
+				Username<input type="text" name="username"/><br/>
+				Password<input type="password" name="password"/><br/>
+			</div>
+		</body>
+	</html>
