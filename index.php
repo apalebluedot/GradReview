b5d44dea4ca0a14a0b84f300c1143fafb99f9b2a
Bradley Barnes <bradley.barnes21@gmail.com>
2013-04-05 00:55:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<!Doctype HTML>
-	<html>
-		<head>
-			<title>GradReview</title>
-			<link rel="stylesheet" href="style.css">
-		</head>
-		<body>
-			<div>
-				Username<input type="text" name="username"/><br/>
-				Password<input type="password" name="password"/><br/>
-			</div>
-		</body>
-	</html>
+
